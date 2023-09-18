d520f22081a12a79ff68b1ef7a1db6bcde37fb67
Ivan Kashuba <kashubaim@gmail.com>
2023-09-18 22:50:24 +0600

    final commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,7 +19,7 @@ class MobileBankApiTestV1 {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello, world!"))
+                .body("data", equalTo("Привет, мир!"))
         ;
     }
 }
